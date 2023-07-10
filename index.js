88f1d9cf7843002579ec727431c06d28fd16aa71
Elizabeth <ek@elizabethkillgore.com>
2023-07-10 17:42:46 -0500

    Array Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+   const win = record.find(object => object.result === "W")
+      if(win === undefined) { 
+        return undefined 
+    } else {
+        return win.year  
+    }
+}
+
